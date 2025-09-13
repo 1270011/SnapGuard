4fe208ae5a49679434cc040ca4a812d838ed2774
127001 <85120225+1270011@users.noreply.github.com>
2025-09-13 20:28:08 +0200

    Update pve-backup-with-mail-wrapper.sh
    add note for required mta

--- pve-backup-with-mail-wrapper.sh
+++ pve-backup-with-mail-wrapper.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# For using the wrapper, you need a working mta on your system for example postfix / sendmail / exim4
 OUTPUT=$(/bin/bash /root/scripts/pve-backup.sh 2>&1)
 echo "$OUTPUT" | tee -a /var/log/pve-backup.log
 echo "$OUTPUT" | mail -s "Proxmox VE Backup $(date +%Y-%m-%d)" pvemonitoring@example.com  # adjust your mail accordingly
